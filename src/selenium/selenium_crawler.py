28771dfd8796b5b9d89a4fbb6c44d81c3a9958bb
Ismael de la Gracia Cobos <ismael.de.la.gracia@estudiantat.upc.edu>
2020-10-28 11:47:42 +0100

    Simple extension example with selenium!

--- src/selenium/selenium_crawler.py
+++ src/selenium/selenium_crawler.py
@@ -1,16 +1,40 @@
-import os
+import os, zipfile
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from pathlib import Path
 
-exe_path = "./chromedriver"
-os.environ["webdriver.chrome.driver"] = exe_path
+exe_path = Path("./chromedriver")
+os.environ["webdriver.chrome.driver"] = str(exe_path)
+
+ext_dir = Path('extension')
+ext_file = Path('extension.zip')
+
+# Create zipped extension
+## Read in your extension files
+file_names = os.listdir(ext_dir)
+file_dict = {}
+for fn in file_names:
+    with open(os.path.join(ext_dir, fn), 'r') as infile:
+        file_dict[fn] = infile.read()
+
+## Save files to zipped archive
+with zipfile.ZipFile(ext_file, 'w') as zf:
+    for fn, content in file_dict.items():
+        zf.writestr(fn, content)
 
 chrome_options = Options()
-chrome_options.add_extension('./extension/download_page.js')
+chrome_options.add_extension(ext_file)
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.experimental_options["useAutomationExtension"] = False
 
-driver = webdriver.Chrome(executable_path=exe_path, chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=exe_path, options=chrome_options)
 driver.get("http://www.python.org")
+
+
+
 # assert "Python" in driver.title
 # elem = driver.find_element_by_name("q")
 # elem.clear()
